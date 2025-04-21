e8b4d64a7a07a1eb62ca7f6ecc91719ac653dc5d
Darya <ara1015@tut.by>
2025-04-21 20:47:05 +0300

    metods get_post_by_id_json and get_all_posts_json added

--- eblog/blog_posts/urls.py
+++ eblog/blog_posts/urls.py
@@ -6,4 +6,6 @@ app_name = 'blog_posts'
 urlpatterns = [
     path('', views.posts_list, name="list"),
     path('<slug:slug>', views.post_page, name="page"),
+    path('api/posts/', views.get_all_posts_json, name='get_all_posts_json'),
+    path('api/posts/<int:post_id>/', views.get_post_by_id_json, name='get_post_by_id_json'),
 ]
--- eblog/blog_posts/views.py
+++ eblog/blog_posts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse, HttpResponse
 from .models import Post
 
 def posts_list(request):
@@ -9,3 +10,31 @@ def posts_list(request):
 def post_page(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'blog_posts/post_page.html', {'post': post})
+
+
+def get_all_posts_json(request):
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        data = [
+            {
+                'id': post.id,
+                'title': post.title,
+                'body': post.body,
+                'slug': post.slug,
+                'date': post.date.isoformat(),
+            }
+            for post in posts
+        ]
+        return JsonResponse(data, safe=False)
+
+def get_post_by_id_json(request, post_id):
+    if request.method == 'GET':
+        post = get_object_or_404(Post, id=post_id)
+        data = {
+            'id': post.id,
+            'title': post.title,
+            'body': post.body,
+            'slug': post.slug,
+            'date': post.date.isoformat(),
+        }
+        return JsonResponse(data)
